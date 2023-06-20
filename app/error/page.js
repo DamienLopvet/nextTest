b26a86f5ca042a2a7b32aa4d5ecfba416f1789c1
Damien Lopvet <damienlopvet@gmail.com>
2023-06-20 06:44:15 +0200

    add auth test add text white

--- app/error/page.js
+++ app/error/page.js
@@ -6,11 +6,11 @@ const [session, setSession] = useState('yes')
 
     if(!session) throw new Error('error customized by Damien')
   return (
-    <div className="p-4">
-      <div className="">page is ok for Now but if you
+    <div className="p-4 text-white">
+      <div className="">This page is ok for Now but if you
       
       </div>
-        <button className="border p-1"
+        <button className="border p-1 m-1 ml-0"
         onClick={() => setSession(null)}
         >click here </button><span>&nbsp; that will throw an error and fall on the error.js file </span>
     </div>
